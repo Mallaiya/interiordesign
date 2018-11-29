f6d8a95ad16f46ddf8ccdd47ec42d0031b608760
Mallaiya M <mallaiyazap@gmail.com>
2018-11-29 10:27:56 +0530

    Updates

---
+++ precache-manifest.195fd93e7119969f3f767010e7b8439a.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f8748f8ce1541064dd227314076786",
+    "url": "/interiordesign/static/media/social-icons.38f8748f.png"
+  },
+  {
+    "revision": "0fd02c1acd4c0b0f1a19",
+    "url": "/interiordesign/static/css/main.643a21a2.chunk.css"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/interiordesign/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "8bf4e79d159e89c9b17a",
+    "url": "/interiordesign/static/js/1.8bf4e79d.chunk.js"
+  },
+  {
+    "revision": "f37fab0eb21a0df3ac02",
+    "url": "/interiordesign/static/js/runtime~main.f37fab0e.js"
+  },
+  {
+    "revision": "d11d150ac7cb9e4161713f0443c186db",
+    "url": "/interiordesign/static/media/galleryImg1.d11d150a.jpg"
+  },
+  {
+    "revision": "43ff4cf2ca07b477290f384e1a6a85f7",
+    "url": "/interiordesign/static/media/galleryImg2.43ff4cf2.jpg"
+  },
+  {
+    "revision": "958046fddcec746e94d1fa8a2571f7e0",
+    "url": "/interiordesign/static/media/galleryImg3.958046fd.jpg"
+  },
+  {
+    "revision": "32e8791a9ff7b785983d7993826219f0",
+    "url": "/interiordesign/static/media/galleryImg4.32e8791a.jpg"
+  },
+  {
+    "revision": "63d78adc01a375090a0c292912249114",
+    "url": "/interiordesign/static/media/galleryImg5.63d78adc.jpg"
+  },
+  {
+    "revision": "258e06fb413abd33da6215757343efc4",
+    "url": "/interiordesign/static/media/galleryImg6.258e06fb.jpg"
+  },
+  {
+    "revision": "eff6ea3a69713d5db5cb5808fc56740d",
+    "url": "/interiordesign/static/media/thumb1.eff6ea3a.jpg"
+  },
+  {
+    "revision": "23fbb1adadaa896067f8b5d7ef1770f3",
+    "url": "/interiordesign/static/media/thumb2.23fbb1ad.jpg"
+  },
+  {
+    "revision": "64026e7fb39573aa6d3405b7cdae150b",
+    "url": "/interiordesign/static/media/thumb4.64026e7f.jpg"
+  },
+  {
+    "revision": "62fa440d0efe1a36ec4cefc0c6783d33",
+    "url": "/interiordesign/static/media/thumb6.62fa440d.jpg"
+  },
+  {
+    "revision": "d0aeea56a44d0e1dce9faaa2f459d89d",
+    "url": "/interiordesign/static/media/service-img1.d0aeea56.jpg"
+  },
+  {
+    "revision": "6b22c02ba696174704cc9a2a6a2a28f7",
+    "url": "/interiordesign/static/media/service-img2.6b22c02b.jpg"
+  },
+  {
+    "revision": "0fd02c1acd4c0b0f1a19",
+    "url": "/interiordesign/static/js/main.0fd02c1a.chunk.js"
+  },
+  {
+    "revision": "da6c28eef569b12022822387e86017e8",
+    "url": "/interiordesign/static/media/bg-top.da6c28ee.jpg"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/interiordesign/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/interiordesign/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/interiordesign/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/interiordesign/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/interiordesign/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/interiordesign/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/interiordesign/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/interiordesign/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/interiordesign/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/interiordesign/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/interiordesign/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/interiordesign/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/interiordesign/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/interiordesign/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "8bf4e79d159e89c9b17a",
+    "url": "/interiordesign/static/css/1.1457839a.chunk.css"
+  },
+  {
+    "revision": "4fc156a074e082c502e49e629f291acf",
+    "url": "/interiordesign/index.html"
+  }
+];
